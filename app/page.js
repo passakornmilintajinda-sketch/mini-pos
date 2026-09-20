9bd7e6ed2cb5a0976dc0ed233dbc5f4a26397572
passakornmilintajinda-sketch <passakornmilintajinda@gmail.com>
2026-09-20 20:36:50 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 const LOW_STOCK_THRESHOLD = 5;
 
